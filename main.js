aa508bd3520d97733a7a93523056013100991c90
Fero737373 <sen.ferhat@gmx.net>
2026-02-02 16:55:14 +0100

    Icon

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
     height: 900,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
     }
   });
 
+  win.maximize();
   win.loadFile('dashboard_problemlandkarte.html');
 }
 
